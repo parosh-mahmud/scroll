db9dd4e3adb20597f67cdd73fe14f38ec885e1d2
Parosh Mahmud <prparosh@gmail.com>
2021-01-16 09:34:03 +0600

    added all files

---
+++ app.js
@@ -0,0 +1,64 @@
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
+
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+
+const links = document.querySelector(".links");
+
+navToggle.addEventListener('click', function () {
+    const linksHeight = links.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`;
+    } else {
+        linksContainer.style.height = 0;
+    }
+    // console.log(linksContainer.getBoundingClientRect());
+});
+
+const navBar = document.getElementById('nav');
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navBar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navBar.classList.add("fixed-nav");
+    } else {
+        navBar.classList.remove("fixed-nav");
+    }
+    //setup back to top link
+    if (scrollHeight > 500) {
+        // console.log("back to top")
+        topLink.classList.add("show-link");
+    } else {
+        topLink.classList.remove("show-link");
+    }
+
+});
+
+// smooth scroll
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        const navHeight = navBar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navBar.classList.contains("fixed-nav");
+        let position = element.offsetTop - navHeight;
+        if (!fixedNav) {
+            position = position - navHeight;
+        }
+        if (navHeight > 82) {
+            position = position + containerHeight;
+        }
+        window.scrollTo({
+            left: 0,
+            top: position,
+        });
+        linksContainer.style.height = 0;
+    })
+})
